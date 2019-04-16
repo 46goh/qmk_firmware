ececa6e369420808b9d6d532a972f3eece0a1c27
新上達也 <shinjo@shinjo.local>
2019-04-17 02:12:18 +0900

    ergodash/parasolr modify space as shift + space

--- keyboards/ergodash/rev2/keymaps/parasolr/keymap.c
+++ keyboards/ergodash/rev2/keymaps/parasolr/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Ctrl |                    |  Alt |   N  |   M  |   ,  |   .  |   /  |  Up  |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * |      |      | Ctrl |  GUI |||||||| Lower| Shift| Space|||||||| Enter| BkSp | Raise||||||||  GUI | Left | Down | Right|
+   * |      |      | Ctrl |  GUI |||||||| Lower| Shift|Sft+Sp|||||||| Enter| BkSp | Raise||||||||  GUI | Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_QUOT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,                        KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   \
-    _______, _______, KC_LCTL, KC_LGUI,          LOWER,   KC_LSFT,  KC_SPC,       KC_ENT, KC_BSPC, RAISE,            KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT  \
+    _______, _______, KC_LCTL, KC_LGUI,          LOWER,   KC_LSFT,  LSFT(KC_SPC), KC_ENT, KC_BSPC, RAISE,            KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Lower
