d0fd7af207a8fb42902a27adb9081247efa6ad92
parasolr <palcum@gmail.com>
2019-04-18 01:28:57 +0900

    ergodash/parasolr Shift + Enter does not match with IME

--- keyboards/ergodash/rev2/keymaps/parasolr/keymap.c
+++ keyboards/ergodash/rev2/keymaps/parasolr/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Ctrl |                    |  Alt |   N  |   M  |   ,  |   .  |   /  |  Up  |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * |      |      |Ct+GUI|  GUI |||||||| Shift| Lower|Sft+Sp|||||||| Enter| Raise| Bksp ||||||||  GUI | Left | Down | Right|
+   * |      |      |Ct+GUI|  GUI |||||||| Shift| Lower| Space|||||||| Enter| Raise| Bksp ||||||||  GUI | Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_QUOT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     LCTL(KC_LSFT), KC_A, KC_S, KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,                        KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   \
-    _______, _______, LCTL(KC_LGUI), KC_LGUI,    KC_LSFT, LOWER,   LSFT(KC_SPC),  KC_ENT, RAISE,   KC_BSPC,          KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT  \
+    _______, _______, LCTL(KC_LGUI), KC_LGUI,    KC_LSFT, LOWER,   KC_SPC,        KC_ENT, RAISE,   KC_BSPC,          KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Lower
