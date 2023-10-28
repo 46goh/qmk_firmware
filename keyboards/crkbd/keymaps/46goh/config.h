b9ed48eceefbcb6b04e940025be162aab7895c26
46goh <4690goh@gmail.com>
2023-10-28 22:21:07 +0900

    enable rgb-rainbow animation again

--- keyboards/crkbd/keymaps/46goh/config.h
+++ keyboards/crkbd/keymaps/46goh/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef RGBLIGHT_ENABLE
 #    undef RGBLED_NUM
-#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 #    define RGBLED_NUM 54
 #    undef RGBLED_SPLIT
 #    define RGBLED_SPLIT \
